eca0601aef7b9d6f0b2df1a408647382bb58e4c0
anna <annalebedeva0911@gmail.com>
2026-03-11 09:05:25 +0700

    lab5: fix test context load

--- lab3/src/test/kotlin/com/example/lab3/Lab3ApplicationTests.kt
+++ lab3/src/test/kotlin/com/example/lab3/Lab3ApplicationTests.kt
@@ -1,13 +1,9 @@
 package com.example.lab3
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class Lab3ApplicationTests {
-
     @Test
     fun contextLoads() {
     }
-
 }
